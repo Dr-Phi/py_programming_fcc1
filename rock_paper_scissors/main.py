8d433d347c1eb1dbe614ffc913200f3b992e94e8
Dr-Phi <eliasrojaslon@gmail.com>
2022-08-09 12:30:17 -0500

    1st commit: juego con comentarios sobre funciones y refactorización; falta darle la lógica final al llamar las funciones

---
+++ rock_paper_scissors/main.py
@@ -0,0 +1,36 @@
+import random
+
+def get_choices(): # Function with no parameters
+  options = ["rock", "paper", "scissors"]
+  player_choice = input("Write your choice (rock, paper, scissors): ")
+  computer_choice = random.choice(options)
+  choices = {
+  "player": player_choice,
+  "computer": computer_choice
+  }
+  return choices
+
+def check_win(player, computer): # Function with 2 parameters
+  print(f"You chose {player}, computer chose {computer}")
+  if player == computer:
+    print("It's a tie!")
+  elif player == "scissors" and computer == "rock":
+    print("Computer wins.")
+  elif player == "scissors" and computer == "paper":
+    print("Player wins!")
+  elif player == "rock": # 1st refactor of code using nested if/elif
+    if computer == "scissors":
+      print("Player wins!")
+    elif computer == "paper":
+      print("Computer wins.")
+  elif player == "paper": # 2nd refactor of code using nested if/else
+    if computer == "rock":
+      print("Player wins")
+    else:
+      print("Computer wins.")
+  else:
+    print("Write a valid choice")
+
+input("Welcome to my first RPS game.\npress Enter")
+
+check_win("rock", "rock")
